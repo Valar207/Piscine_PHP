d55c42d47a7873c6f302c722dbf10e3b793622ba
Valentin <val.rossi31795@gmail.com>
2019-10-09 20:06:56 +0200

    save

---
+++ d01/ex02/oddeven.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+class Car {
+    function Car() {
+        $this->model = "V§èikuyghW";
+    }
+}
+// create an object
+$herbie = new Car();
+
+// show object properties
+echo $herbie->model;
+
+
+
+?>
