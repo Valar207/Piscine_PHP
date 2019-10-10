880fb61a1ebbae11c4ff7cefcc936e8d01aa5072
Valentin ROSSI <vrossi@e2r5p15.42.fr>
2019-10-10 17:56:37 +0200

    save

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,45 +1,22 @@
 #!/usr/bin/php
 <?php
-
-
-
-
-
 function    ft_is_sort($tab)
 {
+    $cmp = $tab;
+    sort($cmp);
     $i = 0;
-    while ($tab[$i])
+    $res;
+    while($cmp[$i])
     {
-        if ($tab[$i] > $tab[$i + 1])
-            return (false);
-        else
-            return (true);
+        if ($cmp[$i] != $tab[$i])
+            $res = 0;
+        else 
+            $res = 1;
         $i++;
     }
+    if ($res == 1)
+        return (true);
+    else
+        return (false);
 }
-
-
-
-
-
-$tab = array("!/@#;^", "42", "Hello World", "salut", "zZzZzZz");
-$tab[] = "Et qu’est-ce qu’on fait maintenant ?";
-
-$i = 0;
-while ($tab[$i])
-{   
-    echo "$tab[$i] ";
-    $i++;
-}
-
-echo "\n";
-$t = ft_is_sort($tab);
-
-echo "$t\n";
-
-if (ft_is_sort($tab))
-echo "Le tableau est trie\n";
-else
-echo "Le tableau n’est pas trie\n";
-
 ?>
