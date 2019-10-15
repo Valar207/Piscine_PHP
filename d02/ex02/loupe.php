396aac6b1711cd4556ad05add0dac40ced7b4124
Valentin ROSSI <vrossi@e2r8p7.42.fr>
2019-10-15 14:24:41 +0200

    save

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -1,46 +1,15 @@
 #!/usr/bin/php
-<?php
-function ft_strtoupper_t($m)
+<?PHP
+$file = file_get_contents($argv[1]);
+function upper1($file)
 {
-	$t = strstr($m, ">", 1);
-	$t = strtoupper($t);
-	$t = $t . strstr($m, ">");
-	return($t);
-}
-function ft_struper($m)
+    return ($file[1].strtoupper($file[2]).$file[3].strtoupper($file[4]));
+} 
+function upper2($file)
 {
-	$t = strstr($m, ">");
-	$t = strtoupper($t);
-	$e = strstr($m, ">", 1) . $t;
-	return ($e);
+    return ($file[1].strtoupper($file[2]).$file[3]);
 }
-function upper($match)
-{
-	return strtoupper($match[0]);
-}
-if ($argc > 1)
-{
-	$file = file_get_contents($argv[1]);
-	$fd = explode('title', $file);
-	$i = count($fd) - 1;
-	$f = 0;
-	while ($f <= $i) {
-		if (preg_match("/\s{0,}=/", $fd[$f]) >= 1)
-			{
-				$fd[$f] = ft_strtoupper_t($fd[$f]);
-			}
-		$f++;
-	}
-	$test = implode('title', $fd);
-	$te = explode('<', $test);
-	$i = count($te);
-	$f = 0;
-	while ($f != $i) {
-		if (strstr($te[$f], "href") != FALSE)
-			$te[$f] = ft_struper($te[$f]);
-		$f++;
-	}
-	$test = implode('<', $te);
-	echo $test;
-}
+$r = preg_replace_callback('/(<a.*title=")(.*)(">)(.*<\/)/', 'upper1', $file);
+$res = preg_replace_callback('/(<a .*>)(.*)(<img)/', 'upper2', $r);
+echo "$res\n";
 ?>
