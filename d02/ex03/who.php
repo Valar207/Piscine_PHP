594bb1948b53dee6037b2972e3a07ca04e359153
Valentin ROSSI <vrossi@e2r8p7.42.fr>
2019-10-15 17:54:11 +0200

    save

---
+++ d02/ex03/who.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+date_default_timezone_set('Europe/paris');
+$file = fopen("/var/run/utmpx", "r");
+while (!feof($file))
+{
+    $res = fread($file, 628);
+    if (strlen($res) == 628)
+    {
+        $res = unpack("a256user/a4id/a32line/ipid/itype/itime", $res);
+        if ($res['type'] == 7)
+        {
+            echo trim($res['user'])."   ";
+            echo trim($res['line'])."  " ;
+            echo (date("M d H:i", $res['time']))."\n";
+        }
+    }
+}
+?>
