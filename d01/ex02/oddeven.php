2ede323b2a053f7ec1915f2a944ffd979283b917
Valentin <val.rossi31795@gmail.com>
2019-11-07 23:49:48 +0100

    save ex02 d01 correct

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -1,18 +1,17 @@
 #!/usr/bin/php
 <?php
-while (1)
-{
+while(1){
     echo "Entrez un nombre: ";
-    $input = trim(fgets(STDIN));
-    if (is_numeric($input))
-    {
-        echo "Le chiffre $input est ";
-        if ($input % 2 == 0)
-        echo "Pair\n";
+    $nb = trim(fgets(STDIN));
+    if (feof(STDIN) == TRUE)
+        exit();
+    if(is_numeric($nb)){
+        if ($nb % 2 == 0)
+            echo "Le chiffre $nb est Pair\n";
         else
-        echo "Impair\n";
+            echo "Le chiffre $nb est Impair\n";
     }
-    else 
-    echo "'$input' n'est pas un chiffre\n";
+    else
+        echo "'$nb' n'est pas un chiffre\n";
 }
 ?>
