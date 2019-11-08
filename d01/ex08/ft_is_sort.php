907147f2156d8190ba330c70cd73d6fa6b9e9b20
Valentin <val.rossi31795@gmail.com>
2019-11-08 12:42:55 +0100

    save ex08 correct

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -5,14 +5,34 @@ function    ft_is_sort($tab)
     $cmp = $tab;
     sort($cmp);
     $i = 0;
+    $j = 0;
     $res;
-    while($cmp[$i])
-    {
-        if ($cmp[$i] != $tab[$i])
-            $res = 0;
-        else 
-            $res = 1;
-        $i++;
+    if (($cmp[$i] == $tab[$i])){
+        while($i < count($cmp))
+        {
+            if ($cmp[$i] != $tab[$i]){
+                $res = 0;
+                break;
+            }
+            else 
+                $res = 1;
+            $i++;
+        }
+    }
+    else{
+        while($cmp[$i + 1])
+            $i++;
+        while($i != 0)
+        {
+            if ($cmp[$i] != $tab[$j]){
+                $res = 0;
+                break;
+            }
+            else 
+                $res = 1;
+            $i--;
+            $j++;
+        } 
     }
     if ($res == 1)
         return (true);
