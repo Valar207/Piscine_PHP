19cc9a25a338aa4f1640d6a1caee28a6e02599f5
Valentin <val.rossi31795@gmail.com>
2019-11-12 20:01:41 +0100

    save d03 correct

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,8 +1,11 @@
 <?php
 if ($_GET["action"] == "set")
 	setcookie($_GET["name"], $_GET["value"], time() + 3600);
-else if ($_GET["action"] == "get")
-	echo $_COOKIE[$_GET["name"]] . "\n";
+else if ($_GET["action"] == "get"){
+	echo $_COOKIE[$_GET["name"]];
+	if ($_COOKIE[$_GET["name"]] != NULL)
+		echo "\n";
+}
 else if ($_GET["action"] == "del")
-    setcookie($_GET["name"], '', 0);
+	setcookie($_GET["name"], '', time() - 1);
 ?>
